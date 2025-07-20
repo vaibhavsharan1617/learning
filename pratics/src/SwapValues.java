de699cc276f3231ee320c3497e05725cbadc3b3b
Pradeepkumar123lens <pradeepkumar802120@gmail.com>
2025-07-20 20:42:00 +0530

    Initial commit...

--- pratics/src/SwapValues.java
+++ pratics/src/SwapValues.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class swap {
-}
+public class SwapValues {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" Write your age");
+        int marks = sc.nextInt();
+        if(marks >=90){
+            System.out.println( "You got A grade");
+        }
+        if (marks <=60){
+            System.out.println();
+        }
+
+        }
+        else
+            System.out.println("you are not eligible for vote");
+        }
+    }
